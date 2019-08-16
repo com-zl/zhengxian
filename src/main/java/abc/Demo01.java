4a6b23571ca502292a71a04f0f8505fdbda5e81f
djxwl <1907843302@qq.com>
2019-08-16 16:58:45 +0800

    djx第一次修改

--- src/main/java/abc/Demo01.java
+++ src/main/java/abc/Demo01.java
@@ -3,7 +3,7 @@ package abc;
 public class Demo01 {
 
 	public static void main(String[] args) {
-		System.out.println(1);
-		System.out.println("djx,2");
+		System.out.println(12);
+		System.out.println(2);
 	}
 }
