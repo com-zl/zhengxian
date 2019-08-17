fc7e040bcda9b62da1a51483dc84021b422445a4
hh <hh@DESKTOP-8OULFKS>
2019-08-17 17:54:30 +0800

    创建了PurchaseController

---
+++ src/main/java/com/zl/st/controller/hh/PurchaseController.java
@@ -0,0 +1,26 @@
+package com.zl.st.controller.hh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class PurchaseController {
+
+	//查询所有
+	public String listFindAll(Model model) {
+		
+		return "";
+	}
+	
+	
+	
+	public String listFindExhibition() {
+		
+		return "";
+	}
+	
+	
+	
+}
