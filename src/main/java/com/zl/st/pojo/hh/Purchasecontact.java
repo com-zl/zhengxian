d9f03913f4a1afed689169bb45b8e4546e515160
hh <hh@DESKTOP-8OULFKS>
2019-08-19 20:53:51 +0800

    增加了采购联系人实体类

---
+++ src/main/java/com/zl/st/pojo/hh/Purchasecontact.java
@@ -0,0 +1,58 @@
+package com.zl.st.pojo.hh;
+
+import java.io.Serializable;
+
+public class Purchasecontact implements Serializable {
+
+	private Integer purchaserid;
+	private String username;
+	private String userpwd;
+	private String nickname;//
+	private Integer buyersid;//采购商id PS:这个ID是证明采购商的
+	private String telphone;
+	private String fulladdress;//详细地址
+	public Integer getPurchaserid() {
+		return purchaserid;
+	}
+	public void setPurchaserid(Integer purchaserid) {
+		this.purchaserid = purchaserid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public Integer getBuyersid() {
+		return buyersid;
+	}
+	public void setBuyersid(Integer buyersid) {
+		this.buyersid = buyersid;
+	}
+	public String getTelphone() {
+		return telphone;
+	}
+	public void setTelphone(String telphone) {
+		this.telphone = telphone;
+	}
+	public String getFulladdress() {
+		return fulladdress;
+	}
+	public void setFulladdress(String fulladdress) {
+		this.fulladdress = fulladdress;
+	}
+	
+	
+}
